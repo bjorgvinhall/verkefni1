5787df49a2a16540ddcddbc1bbfe0b2053dbc24a
bjorgvinhall <43784017+bjorgvinhall@users.noreply.github.com>
2019-01-25 15:42:49 +0000

    eslint og errorhandling

--- lectures.js
+++ lectures.js
@@ -9,7 +9,7 @@ const readFile = util.promisify(fs.readFile);
 
 const router = express.Router();
 
-async function lesaskra(){
+async function lesaskra() {
   const skra = await readFile('./lectures.json');
 
   const json = JSON.parse(skra);
@@ -22,27 +22,29 @@ function catchErrors(fn) {
   return (req, res, next) => fn(req, res, next).catch(next);
 }
 
-//lesa fyrirlestrana inn og birta
+// lesa fyrirlestrana inn og birta
 async function list(req, res) {
   const data = await lesaskra();
   const title = 'Fyrirlestrar';
   const { lectures } = data;
 
-  res.render('index', { title, lectures});
+  res.render('index', { title, lectures });
 }
 
-async function lecture(req, res, next) {
+async function lecture(req, res, next) { // eslint-disable-line
   const { slug } = req.params;
   const data = await lesaskra();
   const foundContent = data.lectures.find(a => a.slug === slug);
   if (!foundContent) {
     return next();
   }
-  
+
   const { title } = foundContent;
   const { category } = foundContent;
   const html = item.createContent(foundContent.content);
-  res.render('lectures', { title, html, category, lecture: foundContent });
+  res.render('lectures', {
+    title, html, category, lecture: foundContent,
+  });
 }
 
 router.get('/', catchErrors(list));
